6d582127671ae90b8a5876d4eab0b832f546967f
whsasf <whsasf@126.com>
2018-05-23 17:50:19 +0800

    new

--- example1.py
+++ example1.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+# coding=utf-8  
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 browser = webdriver.Chrome()
 
-browser.get('https://www.yahoo.com')
-assert 'Yahoo' in browser.title
+browser.get('https://www.baidu.com')
+assert '百度一下，你就知道' in browser.title
 
-elem = browser.find_element_by_name('p')  # Find the search box
+elem = browser.find_element_by_name('wd')  # Find the search box
 elem.send_keys('seleniumhq' + Keys.RETURN)
-
+time.sleep(2)
+browser.back()
+time.sleep(2)
+browser.forward()
+time.sleep(2)
 browser.quit()
